d782d2ac84417c713c9da2b7f24de59296293030
MohamedElshahat55 <mohamedelshahat582@gmail.com>
2024-10-14 23:52:18 +0300

    register formBuilder to our register comp.

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  constructor(private _fb: FormBuilder) {}
+
+  form = this._fb.nonNullable.group({
+    username: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
+}
